8c0187d3cd8632d6e1e2156a95660d976b9b74b2
Jsegel <segeljosh@gmail.com>
2020-01-20 15:30:24 -0600

    adding employee constructor

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "What is your role?",
+            choices: [
+                ""
+            ]
+        }
+    ])

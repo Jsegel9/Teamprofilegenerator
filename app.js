568e37f5b66a428f0f0e565ab0ccbd2142e5cdbe
Jsegel <segeljosh@gmail.com>
2020-01-20 19:11:59 -0600

    .

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 const Employee = require("./employee");
 
-inquirer
-    .prompt([
-        {
-            type: "list",
-            name: "role",
-            message: "What is your role?",
-            choices: [
-                ""
-            ]
-        }
-    ])
+// inquirer
+//     .prompt([
+//         {
+//             type: "list",
+//             name: "role",
+//             message: "What is your role?",
+//             choices: [
+//                 ""
+//             ]
+//         }
+//     ])

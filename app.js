5bb1b9ccf7f2b047529c2053268c265aeade109c
Jsegel <segeljosh@gmail.com>
2020-01-20 21:47:17 -0600

    working on team constructor

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const Employee = require("./employee");
+const Intern = require("./intern");
+const Engineer = require("./engineer");
+const Manager = require("./manager");
 
-// inquirer
-//     .prompt([
-//         {
-//             type: "list",
-//             name: "role",
-//             message: "What is your role?",
-//             choices: [
-//                 ""
-//             ]
-//         }
-//     ])
+class CreateTeam{
+    constructor() {
+
+    }
+    enterEmployee(){
+        return inquirer
+            .prompt([
+                {
+                    type: "confirm",
+                    name: "start",
+                    message: "Would you like to add an employee to your team?"
+                }
+            ])
+            .then(function(response){
+                console.log(response);
+            })
+    }
+}
+
+const team = new CreateTeam();
+team.enterEmployee();
+
+// enterEmployee = function() {
+//     return inquirer
+//         .prompt([
+//             {
+//                 type: "confirm",
+//                 name: "start",
+//                 message: "Would you like to add an employee to your team?"
+//             }
+//         ])
+//         .then(function(response){
+//             console.log(response);
+//         })
+
+// }
+
+// enterEmployee();
